6c125e84893c9bace9a51bcab4af12aeaaf35a55
Deathvks <dylanjesussuarez@gmail.com>
2023-11-23 23:14:46 +0000

    fixed bugs

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,30 +1,30 @@
 function initializeMenu() {
-    const HEADER_MENU_ICON = document.getElementById("header-menu-icon");
-    HEADER_MENU_ICON.addEventListener("click", showHideMenu);
+  const HEADER_MENU_ICON = document.getElementById("header-menu-icon");
+  HEADER_MENU_ICON.addEventListener("click", showHideMenu);
 
-    const SIDE_MENU_ICON = document.getElementById("side-menu-icon");
-    SIDE_MENU_ICON.addEventListener("click", hideSideMenu);
+  const SIDE_MENU_ICON = document.getElementById("side-menu-icon");
+  SIDE_MENU_ICON.addEventListener("click", hideSideMenu);
 
-    const SIDE_MENU = document.getElementById("side-menu");
-    const OVERLAY = document.getElementById("overlay");
+  const SIDE_MENU = document.getElementById("side-menu");
+  const OVERLAY = document.getElementById("overlay");
 
-    document.addEventListener("click", function(event) {
-        const isClickInsideMenu = SIDE_MENU.contains(event.target);
-        const isHeaderMenuIcon = event.target === HEADER_MENU_ICON;
-        if (!isClickInsideMenu && !isHeaderMenuIcon) {
-            hideSideMenu();
-        }
-    });
-
-    function showHideMenu() {
-        SIDE_MENU.style.visibility = "visible";
-        OVERLAY.style.display = "block"; // Mostrar la capa de fondo
+  document.addEventListener("click", function (event) {
+    const isClickInsideMenu = SIDE_MENU.contains(event.target);
+    const isHeaderMenuIcon = event.target === HEADER_MENU_ICON;
+    if (!isClickInsideMenu && !isHeaderMenuIcon) {
+      hideSideMenu();
     }
+  });
 
-    function hideSideMenu() {
-        SIDE_MENU.style.visibility = "hidden";
-        OVERLAY.style.display = "none"; // Ocultar la capa de fondo
-    }
+  function showHideMenu() {
+    SIDE_MENU.classList.add("visible"); // Add the class to show the side-menu
+    OVERLAY.style.display = "block"; // Show background layer
+  }
+
+  function hideSideMenu() {
+    SIDE_MENU.classList.remove("visible"); // Remove the class to hide the side-menu
+    OVERLAY.style.display = "none"; // Hide background layer
+  }
 }
 
 initializeMenu();
