de23057b91f1f910716a7cae374eb4dd9d789bd2
Deathvks <dylanjesussuarez@gmail.com>
2023-11-24 16:47:18 +0000

    add logout

--- js/form-validation.js
+++ js/form-validation.js
@@ -3,8 +3,9 @@ const LOGIN_FORM = document.querySelector("#login form");
 LOGIN_FORM.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  // Validamos el nombre de usuario
+  // Validate the username
   const USERNAME_INPUT = LOGIN_FORM.querySelector("#username");
+  const userEntered = USERNAME_INPUT.value.trim();
   if (USERNAME_INPUT.value === "") {
     document.getElementById("error-user").classList.remove("d-none");
     return;
@@ -12,7 +13,7 @@ LOGIN_FORM.addEventListener("submit", function(event) {
     document.getElementById("error-user").classList.add("d-none");
   }
 
-  // Validamos el email
+  // Validate the email
   const EMAIL_INPUT = LOGIN_FORM.querySelector("#email");
   if (!/\w+@\w+\.\w+/.test(EMAIL_INPUT.value)) {
     document.getElementById("error-email").classList.remove("d-none");
@@ -21,7 +22,7 @@ LOGIN_FORM.addEventListener("submit", function(event) {
     document.getElementById("error-email").classList.add("d-none");
   }
 
-  // Validamos la contraseña
+  // Validate de password
   const PASSWORD_INPUT = LOGIN_FORM.querySelector("#pwd");
   if (PASSWORD_INPUT.value === "") {
     document.getElementById("error-pwd").classList.remove("d-none");
@@ -30,7 +31,7 @@ LOGIN_FORM.addEventListener("submit", function(event) {
     document.getElementById("error-pwd").classList.add("d-none");
   }
 
-  // Validamos la localidad
+  // Validate the location
   const LOCATION_INPUT = LOGIN_FORM.querySelector("#location");
   if (LOCATION_INPUT.value === "") {
     document.getElementById("error-loc").classList.remove("d-none");
@@ -39,8 +40,23 @@ LOGIN_FORM.addEventListener("submit", function(event) {
     document.getElementById("error-loc").classList.add("d-none");
   }
 
-  // Si todas las validaciones son correctas, redireccionamos a otra página web
-  location.href = "../index.html";
+  const usernameValid = USERNAME_INPUT.value !== "";
+  const emailValid = /\w+@\w+\.\w+/.test(EMAIL_INPUT.value);
+  const passwordValid = PASSWORD_INPUT.value !== "";
+  const locationValid = LOCATION_INPUT.value !== "";
+
+  if (usernameValid && emailValid && passwordValid && locationValid) {
+    // Change the h1 text to "Registered"
+    localStorage.setItem('validation', 'valid');
+    document.getElementById("login").innerText = `¡Hola, ${userEntered}!`;
+    document.getElementById("login").style.textAlign = 'center'; // Add this line to center the text  
+
+    // Redirect after a while
+    setTimeout(() => {
+      location.href = "../index.html";
+    }, 2000); // Change this if you want to redirect after a specific time
+  }
 });
 
 LOGIN_FORM();
+
---
+++ js/login.js
@@ -0,0 +1,16 @@
+function logIn(){
+window.onload = function () {
+    // Verificar el estado de la validación
+    if (localStorage.getItem('validation') === 'valid') {
+        // Cambiar el texto del elemento h1
+        document.querySelector('h1').innerText = 'Registrado';
+    }
+}
+
+window.addEventListener('beforeunload', function () {
+    // Limpiar el estado de la validación
+    localStorage.removeItem('validation');
+});
+}
+
+logIn();
