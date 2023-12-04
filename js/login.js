60043417bbbe24a0594b9d4d368824f4465e06c4
Deathvks <dylanjesussuarez@gmail.com>
2023-12-04 20:42:26 +0000

    fixed code

--- js/login.js
+++ js/login.js
@@ -1,16 +1,17 @@
-function logIn(){
-window.onload = function () {
+function logIn() {
+  window.onload = function () {
     // Verificar el estado de la validación
     if (localStorage.getItem('validation') === 'valid') {
-        // Cambiar el texto del elemento h1
-        document.querySelector('h1').innerText = 'REGISTRADO';
+      // Cambiar el texto del elemento h1
+      document.querySelector('h1').innerText = 'REGISTRADO';
     }
-}
+  }
 
-window.addEventListener('beforeunload', function () {
+  window.addEventListener('beforeunload', function () {
     // Limpiar el estado de la validación
-    localStorage.removeItem('validation');
-});
+    // localStorage.removeItem('validation');
+  });
 }
 
 logIn();
+
