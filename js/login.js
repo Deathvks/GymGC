4db680def1649d4fd5084842c5a3535bec80851f
Deathvks <dylanjesussuarez@gmail.com>
2023-12-05 19:06:29 +0000

    fixed bugs

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 function logIn() {
-  window.onload = function() {
+  window.onload = function () {
     // Check if validation status exists in LocalStorage
     const validationStatus = localStorage.getItem('validation');
     if (validationStatus === 'valid') {
@@ -11,7 +11,7 @@ function logIn() {
     }
   };
 
-  window.addEventListener('beforeunload', function() {
+  window.addEventListener('beforeunload', function () {
     // Store validation status in LocalStorage only if it's set to 'valid'
     const validationStatus = localStorage.getItem('validation');
     if (validationStatus === 'valid') {
@@ -21,7 +21,7 @@ function logIn() {
 }
 
 // Click event on element h1 with id="logout"
-document.getElementById('logout').addEventListener('click', function() {
+document.getElementById('logout').addEventListener('click', function () {
   // Clean LocalStorage
   localStorage.removeItem('validation');
   console.log("Validation status removed from localStorage");
@@ -29,8 +29,17 @@ document.getElementById('logout').addEventListener('click', function() {
   // Change the text of the h1 element
   document.querySelector('h1').innerText = 'REGISTRARSE';
 
+  document.querySelector('h1').addEventListener('mouseover', function () {
+    this.style.textDecoration = 'underline';
+  });
+
+  document.querySelector('h1').addEventListener('mouseout', function () {
+    this.style.textDecoration = '';
+  });
+
   // Hide element h1
   document.getElementById('logout').style.visibility = 'hidden';
+
 
   const PAGE = document.getElementById('login');
   PAGE.addEventListener('click', OTHER_PAGE);
